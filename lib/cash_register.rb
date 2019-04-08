429e8f1b055af51d8c4b7b2fbf42d7a55e8c40c6
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 01:53:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,6 @@ class CashRegister
   end
   
   def apply_discount
-    binding.pry
     if @discount
       @total *= ((100.0-@discount.to_f)/100.0)
       return "After the discount, the total comes to @#{total}."
