9eace133e8e9668ca90d2d0786700dee166163c2
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 01:57:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
   def apply_discount
     if @discount
       @total *= (100.0-@discount.to_f)/100.0
-      return "After the discount, the total comes to @#{total}."
+      return "After the discount, the total comes to $#{total.to_i}."
     else
       return "There is no discount to apply."
     end
