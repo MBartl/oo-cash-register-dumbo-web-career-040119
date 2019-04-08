8951278ceac5ec3825570953885a748ad9a6859e
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 01:52:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ class CashRegister
     binding.pry
     @discount.to_f
     if @discount
-      @total *= ((100-@discount)/100)
+      @total *= ((100.0-@discount)/100.0)
       return "After the discount, the total comes to @#{total}."
     else
       return "Error: There is no discount to apply"
